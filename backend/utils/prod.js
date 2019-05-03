107356461e51603df0d2eed700d38e1abc5d98aa
Shicheng <szhou42@illinois.edu>
2019-05-03 01:21:03 -0500

    fix same origin request bug

--- backend/utils/prod.js
+++ backend/utils/prod.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    PY_SERVICE_BASE_URL         : "http://199.223.235.202:8000/"
+    PY_SERVICE_BASE_URL         : "http://199.223.235.202:8000/",
     CLIENT_ORIGIN         : "http://199.223.235.202:3000"
 
 }
