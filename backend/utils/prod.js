0367c42ea7dbd41398e8bf12267b8556606ee5b8
Shicheng <szhou42@illinois.edu>
2019-05-06 20:49:51 -0500

    deploy resume search website with feedback augmented search engine

--- backend/utils/prod.js
+++ backend/utils/prod.js
@@ -1,13 +1,12 @@
 
-/*
 module.exports = {
     PY_SERVICE_BASE_URL         : "http://199.223.235.202:8000/",
     CLIENT_ORIGIN         : "http://199.223.235.202:3000"
 
 }
-*/
-
+/*
 module.exports = {
     CLIENT_ORIGIN         : "http://localhost:3000",
     PY_SERVICE_BASE_URL         : "http://localhost:8000/"
 }
+*/
--- src/utils/prod.js
+++ src/utils/prod.js
@@ -1,10 +1,10 @@
-/*
 
 export const BASE_URL = "http://199.223.235.202:5000/api/"
 export const BASE_URL_WITHOUT_API = "http://199.223.235.202:5000/"
 export const BASE_URL_REACT = "http://199.223.235.202:3000/"
-*/
+/*
 
 export const BASE_URL = "http://localhost:5000/api/"
 export const BASE_URL_WITHOUT_API = "http://localhost:5000/"
 export const BASE_URL_REACT = "http://localhost:3000/"
+*/
